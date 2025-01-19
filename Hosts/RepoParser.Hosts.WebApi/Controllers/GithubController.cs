ab1f1713f7ebc4bf426870230f11ddd742609336
wela <wela@netcompany.com>
2025-01-19 14:12:19 +0100

    Add GithubController

---
+++ Hosts/RepoParser.Hosts.WebApi/Controllers/GithubController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RepoParser.Hosts.WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+public class GithubController: ControllerBase
+{
+    [HttpGet]
+    public Task Parse(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
